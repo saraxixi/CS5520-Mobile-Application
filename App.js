58629296ae0af19c24940cb978b7fd4d3f65a0b5
saraxixi <saraxi.xixi@gmail.com>
2024-09-12 12:40:14 -0700

    Added TextInput component

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './components/Header';
+import { useState } from 'react';
 
 export default function App() {
   const appName = "My awesome app"
+  const [text, setText] = useState("");
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header name = {appName}/>
+      <TextInput 
+        placeholder='Type something' 
+        keyboardType='default' 
+        style={{borderBottomColor: 'purple', borderBottomWidth: 2}}
+        value={text}
+        onChangeText = {function (onChangeText) {setText(onChangeText)}}
+      />
+      <Text>{text}</Text>
     </View>
   );
 }
