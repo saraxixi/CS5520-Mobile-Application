2474dd4f88216ce72c158a850d52acdc5af738ed
saraxixi <saraxi.xixi@gmail.com>
2024-09-12 12:53:42 -0700

    feat: Add Input component for user input

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './components/Header';
+import Input from './components/Input';
 import { useState } from 'react';
 
 export default function App() {
   const appName = "My awesome app"
-  const [text, setText] = useState("");
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header name = {appName}/>
-      <TextInput 
-        placeholder='Type something' 
-        keyboardType='default' 
-        style={{borderBottomColor: 'purple', borderBottomWidth: 2}}
-        value={text}
-        onChangeText = {function (onChangeText) {setText(onChangeText)}}
-      />
-      <Text>{text}</Text>
+      <Input />
     </View>
   );
 }
