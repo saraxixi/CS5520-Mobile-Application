818ba90c909ce84c977ed68676d40319d1202558
saraxixi <saraxi.xixi@gmail.com>
2024-09-12 16:34:10 -0700

    Update Input component to support auto-focus

--- App.js
+++ App.js
@@ -2,16 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './components/Header';
 import Input from './components/Input';
-import { useState } from 'react';
 
 export default function App() {
   const appName = "My awesome app"
 
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Header name = {appName}/>
-      <Input />
+      <StatusBar style = "auto" />
+      <Header name = {appName} />
+      <Input shouldfoucs = {true} />
     </View>
   );
 }
--- components/Input.js
+++ components/Input.js
@@ -2,11 +2,21 @@ import { StyleSheet, TextInput, View, Text, StatusBar } from 'react-native';
 import React from 'react'
 import { useState } from 'react';
 
-export default function Input() {
+export default function Input({ shouldfoucs }) {
   const [text, setText] = useState("");
+
   function updateText(changeText) {
     setText(changeText);
   }
+
+  function characterCount() {
+    if (text.length > 0) {
+      return <Text style = {styles.charCount} > Character Count: {text.length}</Text>
+    } else {
+      return null;
+    }
+  }
+
   return (
     <View>
       <TextInput 
@@ -15,11 +25,12 @@ export default function Input() {
         style={{borderBottomColor: 'purple', borderBottomWidth: 2}}
         value={text}
         onChangeText = {updateText}
+        autoFocus={shouldfoucs}
       />
-        <Text>{text}</Text>
+      <Text>{text}</Text>
+      {characterCount()}
     </View>
-
-  )
+  );
 }
 
 const styles = StyleSheet.create({})
