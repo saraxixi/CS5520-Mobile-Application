3163a87ee542db1fbd1ab43a72511d0f3ba6a9b0
saraxixi <saraxi.xixi@gmail.com>
2024-10-24 11:50:53 -0700

    Refactor GoalUsers.js to add FlatList component and fetch user data from API

--- components/GoalUsers.js
+++ components/GoalUsers.js
@@ -1,17 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 
 export default function GoalUsers() {
+  const [users, setUsers] = useState([]);
+
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        console.log(response.status);
+        if (!response.ok) {
+          throw new Error('HTTP Error happen with status: ' + response.status);
+        }
+        const data = await response.json();
+        console.log(data);
+
+        setUsers(data.map(user => user.name));
+      } catch (err) {
+        console.error('Failed to fetch data:', err);
+      }
     }
     fetchData();
   }, []);
 
   return (
     <View>
-      <Text>GoalUsers</Text>
+      <FlatList
+        data={users}
+        renderItem={({ item }) => (
+          <Text>{item}</Text>
+        )}
+      />
     </View>
   )
 }
