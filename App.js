936a400d717aa10cecdcbdfd16a50aa7173e0160
saraxixi <saraxi.xixi@gmail.com>
2024-10-31 12:11:29 -0700

    Refactor App.js to add authentication support and conditional rendering of screens

--- App.js
+++ App.js
@@ -1,30 +1,23 @@
 import { Button, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Home from './components/Home'
 import GoalDetails from './components/GoalDetails'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Login from './components/Login'
 import Signup from './components/Signup'
+import { auth } from './firebase/firebaseSetup'
+import { onAuthStateChanged } from 'firebase/auth'
 
 const Stack = createNativeStackNavigator();
 
-const commonHeaderStyle = {
-  headerStyle: { backgroundColor: "purple" },
-  headerTintColor: "white"
-}
+const AuthStack = <>
+  <Stack.Screen name="Login" component={Login} />
+  <Stack.Screen name="Signup" component={Signup} />
+</>;
 
-export default function App() {
-  return (
-  <NavigationContainer>
-    <Stack.Navigator
-      screenOptions={commonHeaderStyle}
-      initialRouteName='Login'>
-      
-      <Stack.Screen name="Login" component={Login} options={{title: "Login"}}/>
-      <Stack.Screen name="Signup" component={Signup} options={{title: "Signup"}}/>
-      
-      <Stack.Screen
+const AppStack = <>
+  <Stack.Screen
         name="Home" 
         component={Home} 
         options={{title: "My All Goals"}}
@@ -41,6 +34,34 @@ export default function App() {
           }};
         }}
       />
+</>;
+
+const commonHeaderStyle = {
+  headerStyle: { backgroundColor: "purple" },
+  headerTintColor: "white"
+}
+
+export default function App() {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsUserLoggedIn(true);
+      } else {
+        setIsUserLoggedIn(false);
+      }
+    });
+  }, []);
+
+  return (
+  <NavigationContainer>
+    <Stack.Navigator
+      screenOptions={commonHeaderStyle}
+      initialRouteName='Login'
+    >
+      {
+        isUserLoggedIn ? AppStack : AuthStack
+      }
     </Stack.Navigator>
   </NavigationContainer>
   );
